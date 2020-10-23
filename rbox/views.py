edd1ff0e8378ee7ef615a7adcd10b16bb4e92498
Clive Freeman <clivemeister@gmail.com>
2020-10-23 17:19:36 +0100

    :early changes

--- rbox/views.py
+++ rbox/views.py
@@ -2,3 +2,10 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse('Hello world.  At the index.')
+
+def recipeDetail(request,recipe_id):
+    return HttpResponse("You are looking at receipe %s." % recipe_id)
+
+def recipeList(request):
+    return HttpResponse("You are looking at a list of the receipes")
+    
